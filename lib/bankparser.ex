710936851d1d826543f088d88042699790637443
Jorge Chavez <jchavz@switchfly.com>
2018-11-15 11:39:27 -0700

    Add bankparser module

--- lib/bankparser.ex
+++ lib/bankparser.ex
@@ -1,18 +1,35 @@
 defmodule Bankparser do
-  @moduledoc """
-  Documentation for Bankparser.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Bankparser.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def split_lines(content) do
+    { _last, rest } = content
+    |> String.split(~r{\n})
+    |> List.pop_at(-1)
+    rest
   end
+
+  def remove_header([_head | tail]), do: tail
+
+  def process_float(binary) do
+    binary
+    |> String.replace(",", "")
+    |> String.to_float()
+  end
+
+  def parse_line(line) do
+    [date, reference, _, payment, total] = String.split(line, ~r{\t})
+    %{
+      date: date,
+      reference: reference,
+      payment: payment |> process_float,
+      total: total |> process_float
+    }
+  end
+
+  def parse(content) do
+    content
+    |> split_lines
+    |> remove_header
+    |> Enum.map(fn(line) -> parse_line(line) end)
+  end
+
 end
--- test/bankparser_test.exs
+++ test/bankparser_test.exs
@@ -2,7 +2,44 @@ defmodule BankparserTest do
   use ExUnit.Case
   doctest Bankparser
 
-  test "greets the world" do
-    assert Bankparser.hello() == :world
+  test "line should be parse to a map" do
+    line = "01-04-2018	SPEI RECIBIDOBANAMEX/0005323846  002 0000001  		224.57	88,661.03"
+    assert Bankparser.parse_line(line) == %{
+      date: "01-04-2018",
+      reference: "SPEI RECIBIDOBANAMEX/0005323846  002 0000001  ",
+      payment: 224.57,
+      total: 88661.03
+    }
   end
+
+  test "test float transformation" do
+    assert Bankparser.process_float("22.4") == 22.4
+  end
+
+  test "split lines of file" do
+    case File.read("test/files/bank_file.exp") do
+      {:ok, content} ->
+        assert Bankparser.split_lines(content) == [
+          "D�a	Concepto / Referencia	cargo	Abono	Saldo",
+          "01-04-2018	SPEI RECIBIDOBANAMEX/0005323846  002 0000001		224.57	88,661.03",
+          "02-04-2018	SPEI RECIBIDOBANAMEX/0005315417  002 0000001 		83.52	88,436.46"
+        ]
+      {:error, _reason} ->
+        assert false
+    end
+  end
+
+  test "remove first line" do
+    case File.read("test/files/bank_file.exp") do
+      {:ok, content} ->
+        list = Bankparser.split_lines(content)
+        assert Bankparser.remove_header(list) == [
+          "01-04-2018	SPEI RECIBIDOBANAMEX/0005323846  002 0000001		224.57	88,661.03",
+          "02-04-2018	SPEI RECIBIDOBANAMEX/0005315417  002 0000001 		83.52	88,436.46"
+        ]
+      {:error, _reason} ->
+        assert false
+    end
+  end
+
 end
