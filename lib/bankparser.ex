9edca0c6c165130c81642ce7d7d19e8b75884b22
Jorge Chavez <jchavz@switchfly.com>
2018-11-15 04:35:04 -0700

    Initial commit

---
+++ lib/bankparser.ex
@@ -0,0 +1,18 @@
+defmodule Bankparser do
+  @moduledoc """
+  Documentation for Bankparser.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bankparser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bankparser_test.exs
@@ -0,0 +1,8 @@
+defmodule BankparserTest do
+  use ExUnit.Case
+  doctest Bankparser
+
+  test "greets the world" do
+    assert Bankparser.hello() == :world
+  end
+end
